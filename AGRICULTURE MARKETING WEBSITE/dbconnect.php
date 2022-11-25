6f89e084a15a42ada8c89f780a96ef115f3dd4d4
RAGAVI T <110911541+ragavi-t@users.noreply.github.com>
2022-11-25 09:51:44 +0530

    Add files via upload

---
+++ AGRICULTURE MARKETING WEBSITE/dbconnect.php
@@ -0,0 +1,4 @@
+<?php
+$connect=mysql_connect("localhost","root","") or die("Could not connect!");
+mysql_select_db("farmer_buy",$connect);
+?>
---
+++ AGRICULTURE MARKETING WEBSITE/link_user.php
@@ -0,0 +1,4 @@
+<a href="user_page.php">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="user_parchase_order.php">Products</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="user_purch_view.php">View Cart</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="view_purchase.php">Purchase Details</a>&nbsp;&nbsp;&nbsp;&nbsp;
